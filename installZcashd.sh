ce7f131b88a86edfb5e69397a50237c418fa0ab5
bitcoinbzc <54041244+bitcoinbzc@users.noreply.github.com>
2019-08-14 17:46:03 +0200

    Revert "up"
    This reverts commit a7cbb647d0ab4fae959be4a4dc3578f80f2fbb17.

--- installZcashd.sh
+++ installZcashd.sh
@@ -7,14 +7,12 @@ sudo apt-get install \
       autoconf libtool ncurses-dev unzip git python python-zmq \
       zlib1g-dev wget curl bsdmainutils automake 
 
-# bitzecBitcore
+# zcashBitcore
 cd
 git clone https://github.com/bitzec/bitzec
 cd bitzec
 ./zcutil/fetch-params.sh
 ./zcutil/build.sh --disable-tests -j$(nproc)
-mkdir -p ~/.bitzec
-echo "addnode=bzcseed.raptorpool.org" >~/.bitzec/bitzec.conf
 cd
 echo "Bitzecd with extended RPC functionalities is prepared. Please run following command to install insight explorer for bitzec"
 echo "wget -qO- https://raw.githubusercontent.com/bitzec/bitcore-node-bitzec/master/installExplorer.sh | bash"
